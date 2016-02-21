831553eb3f6102c546dc18bb31423e4346c4659b
Jasper-de-Regt <jderegt@uwo.ca>
2016-02-21 14:52:45 -0500

    commit super rough wall following robot code
    Commit super rough wall following robot code. The code is PID based, but
    not gain tuning was done. The right motor speed is controlled by the
    PID/Ultrasonic sensor. The left motor spoeed is set arbitrarily. This
    would all be done way nicer and smoother, this code is just a rough
    proof of concept.

--- PID_motor_control_code/PID_motor_control_code.ino
+++ PID_motor_control_code/PID_motor_control_code.ino
@@ -1,8 +1,10 @@
+//#include <PID_AutoTune_v0.h>
+
+
 
 
 
 #include <PID_v1.h>
-
 #include <Servo.h>
 #include <uSTimer2.h>
 #include <CharliePlexM.h>
@@ -13,7 +15,10 @@ Servo servo_rightMotor;
 I2CEncoder encoder_rightMotor;
 
 // port pin constants
-const int ci_rightMotor = 8;          // to right motor ESC
+const int ci_enable = 9;          // to enable
+const int ci_fwd = 8;          // to in2
+const int ci_rev = 10;         // to in1
+
 const int ci_Charlieplex_LED1 = 4;    //     CharliePlexM::Write(ci_Middle_Line_Tracker_LED, HIGH);
 const int ci_Charlieplex_LED2 = 5;
 const int ci_Charlieplex_LED3 = 6;
@@ -46,22 +51,24 @@ void setup() {
   CharliePlexM::setBtn(ci_Charlieplex_LED1, ci_Charlieplex_LED2, ci_Charlieplex_LED3, ci_Charlieplex_LED4, ci_Mode_Button);
 
   // set up drive motors
-  pinMode(ci_rightMotor, OUTPUT);
-  servo_rightMotor.attach(ci_rightMotor);
-
+  pinMode(ci_enable, OUTPUT);
+  pinMode(ci_fwd, OUTPUT);
+  pinMode(ci_rev, OUTPUT);
 
   // set up encoders. Must be initialized in order that they are chained together,
   // starting with the encoder directly connected to the Arduino. See I2CEncoder docs
   // for more information
   encoder_rightMotor.init(1.0 / 3.0 * MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
-  encoder_rightMotor.setReversed(false);  // adjust for positive count when moving forward
+  //  encoder_rightMotor.setReversed(false);  // adjust for positive count when moving forward
 
   //initiliaze pid variables
   input = encoder_rightMotor.getPosition();
-  setpoint = 30;
+  setpoint = 1;
+
   myPID.SetMode(AUTOMATIC);
   myPID.SetSampleTime(1);
-  myPID.SetOutputLimits(1570, 2000);
+  myPID.SetOutputLimits(-255, 255);
+
 
   pinMode(proportionalPotPin, INPUT);
   pinMode(derivativePotPin, INPUT);
@@ -74,13 +81,35 @@ void setup() {
 void loop() {
 
 
+  if (CharliePlexM::ui_Btn)
+  {
+    setpoint++;
+  }
 
-  kp = map(analogRead(proportionalPotPin), 0, 1023, 0, 50);
-  ki = map(analogRead(integralPotPin), 0, 1023, 0, 50);
-  kd = map(analogRead(derivativePotPin), 0, 1023, 0, 50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // set charlieplex light on so you know code is uploaded
+  CharliePlexM::Write(1, HIGH);
+
+  // set pid values based on potentiometer values
+  kp = map(analogRead(proportionalPotPin), 0, 1023, 0, 5000);
+  ki = map(analogRead(integralPotPin), 0, 1023, 0, 5000);
+  kd = map(analogRead(derivativePotPin), 0, 1023, 0, 10);
   myPID.SetTunings(kp, ki, kd);
 
-
+  //print pid values, input, output, and setpoint to serial
   Serial.println();
   Serial.print(myPID.GetKp());
   Serial.print("   ");
@@ -96,9 +125,48 @@ void loop() {
 
 
 
-  input = encoder_rightMotor.getSpeed();
+  // getSpeed is a piece of shit; it takes 4 secodns to return 0 after the motor stops
+  // pid control of speed
+  //  input = encoder_rightMotor.getSpeed();
+  //getVelocityBits isn't any better
+  //  input = encoder_rightMotor.getVelocityBits();
+
+  input = encoder_rightMotor.getPosition();
   myPID.Compute();
-  servo_rightMotor.writeMicroseconds(output);
-  CharliePlexM::Write(1, HIGH);
+
+  // output is a value from 0-255
+  // set motor speed to a direction
+
+
+  //  output=map(analogRead(derivativePotPin),0,1023,0,1000);
+  int deadband = 30;
+  
+  if (output == 0) {
+    //do nothing, brake maybe?
+  }
+
+  else  if (output > 0) {
+    digitalWrite(ci_fwd, HIGH);
+    digitalWrite(ci_rev, LOW);
+    int speed = map(output, 0, 255, deadband, 255); //map(output,500,1000,0,255);
+    analogWrite(ci_enable, speed);
+    Serial.print("       dir  ");
+    Serial.print("fwd");
+    Serial.print(" speed  ");
+    Serial.print(speed);
+  }
+  else {
+    digitalWrite(ci_fwd, LOW);
+    digitalWrite(ci_rev, HIGH);
+    int speed = map(output, 0, -255, deadband, 255);
+    //    int speed = map(output, 500, 0, 0, 255);
+    analogWrite(ci_enable, speed);
+    Serial.print("       dir  ");
+    Serial.print("rev");
+    Serial.print(" speed  ");
+    Serial.print(speed);
+  }
+
+
 
 }
