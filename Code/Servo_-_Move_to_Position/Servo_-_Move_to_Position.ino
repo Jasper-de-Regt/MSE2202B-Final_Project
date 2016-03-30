24fc247b3df805c0eaefe64d35accd4245c0a0a8
codiopathy <christophern.bayley@gmail.com>
2016-03-29 21:47:56 -0400

    Minor updates

---
+++ Code/Servo_-_Move_to_Position/Servo_-_Move_to_Position.ino
@@ -0,0 +1,100 @@
+#include <Servo.h>
+
+Servo serv0;
+int delayBy = 10; //Works as low as 2 (for speed).
+int servoPin1 = 7;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  serv0.attach(servoPin1);
+
+  //Sets the initial servo position
+  serv0.write(0);
+
+
+  Serial.println(serv0.read());
+
+  if (serv0.read() > 15) {
+    while (serv0.read() > 15) {
+      serv0.write(serv0.read() - 1);
+    }
+  }
+  
+  
+}
+
+void loop() {
+  /*serv0.write(serv0.read() - 1);
+  delay(50);
+  Serial.println(serv0.read());*/
+
+  moveToPosn(0, 110, 0);
+  delay(500);
+  moveToPosn(110, 0, 0);
+  //moveToPosnByInc(0, 180, 3, 1);
+  serv0.detach(); Serial.println("Detached");
+
+}
+
+void moveToPosnByInc(int initial, int final, int increment, bool hardStartTrue) {
+  //If the position must start at the initial mark
+  if (hardStartTrue == true) {
+    while (serv0.read() != initial) {
+      serv0.write(initial);
+    }
+  }
+
+  if (initial > final) {
+    while (serv0.read() > final) {
+      serv0.write(serv0.read() - increment);
+      delay(delayBy);
+    }
+  }
+  else if (initial < final) {
+    while (serv0.read() < final) {
+      serv0.write(serv0.read() + increment);
+      delay(delayBy);
+    }
+  }
+
+}
+
+
+
+
+
+void moveToPosn(int initial, int final, bool hardStartTrue) {
+
+
+  //if (serv0.attached() == false) {
+  //  serv0.attach(servoPin1);
+  //}
+
+  //If the position must start at the initial mark
+  if (hardStartTrue == true) {
+    Serial.println("Hard Start");
+    while (serv0.read() != initial) {
+      serv0.write(initial);
+      delay(delayBy);
+    }
+  }
+
+  while (serv0.read() != final) {
+    Serial.println("Movement - MTP");
+    while (serv0.read() > final) {
+      serv0.write(serv0.read() - 1);
+      delay(delayBy);
+    }
+    while (serv0.read() < final) {
+      serv0.write(serv0.read() + 1);
+      delay(delayBy);
+    }
+    Serial.print("Status: "); Serial.println(serv0.read());
+    
+  }
+
+
+
+}
