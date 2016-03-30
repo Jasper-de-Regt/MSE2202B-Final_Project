5e2a342e3f59e501adfde0816d344a7317c8992a
mhende29 <mhende29@uwo.ca>
2016-03-30 19:15:19 -0400

    Added zeroing features
    Arm code should be ready for testing to determine encoder constants!

--- Code/MSE_Project_Arm_Code/MSE_Project_Arm_Code/MSE_Project_Arm_Code.ino
+++ Code/MSE_Project_Arm_Code/MSE_Project_Arm_Code/MSE_Project_Arm_Code.ino
@@ -13,30 +13,25 @@ Servo servo_ArmMotor;
 I2CEncoder encoder_TurntableMotor;
 I2CEncoder encoder_ArmMotor;
 
-//#define DEBUG_ARMTURNTABLE_ENCODERS
+#define DEBUG_ARM_TURNTABLE_ENCODERS
 
 //port pin constants
 
-const int ci_TurnTable_Motor = ?;
-const int ci_Arm_Motor = ?;
+const int ci_TurnTable_Motor = 7;
+const int ci_Arm_Motor = 8;
 const int ci_I2C_SDA = A4;         // I2C data = white
 const int ci_I2C_SCL = A5;         // I2C clock = yellow
 
-const int ci_Turntable_Motor_Left = ?;         
-const int ci_Turntable_Motor_Right = ?;       
-const int ci_Arm_Motor_Retracted = ?;      
-const int ci_Arm_Motor_Vertical = ?;      
-
-const int ci_Turntable_Left_Position = ?;      // Experiment to determine appropriate value
-const int ci_Turntable_Middle_Position = ?;    //  "
-const int ci_Turntable_Right_Position = ?;     //  "
-const int ci_Arm_Vertical_Position = ?;        //  "
-const int ci_Arm_Extended_Position = ?;        //  "
+//Position constants
+const int ci_Turntable_Left_Position = 0;       // Experiment to determine appropriate value
+const int ci_Turntable_Middle_Position = 70;    //  "
+const int ci_Turntable_Right_Position = 140;    //  "
+const int ci_Arm_Vertical_Position = 0;         //  "
+const int ci_Arm_Extended_Position = 90;        //  "
 
 void setup() {
   Wire.begin();        // Wire library required for I2CEncoder library
   Serial.begin(9600);
-
 
   // set up arm motor and turntable motor
   pinMode(ci_TurnTable_Motor, OUTPUT);
@@ -48,16 +43,19 @@ void setup() {
   encoder_TurntableMotor.setReversed(false);  // adjust for positive count when moving forward
   encoder_ArmMotor.init(1.0 / 3.0 * MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
   encoder_ArmMotor.setReversed(true);  // adjust for positive count when turning clockwise
+
+  encoder_TurntableMotor.zero();
+  encoder_ArmMotor.zero();
 }
 
 void loop() {
 #ifdef DEBUG_ARM_TURNTABLE_ENCODERS
-          l_Turntable_Motor_Position = encoder_TurntableMotor.getRawPosition();
-          l_Arm_Motor_Position = encoder_ArmMotor.getRawPosition();
+  l_Turntable_Motor_Position = encoder_TurntableMotor.getRawPosition();
+  l_Arm_Motor_Position = encoder_ArmMotor.getRawPosition();
 
-          Serial.print("Encoders Turntable: ");
-          Serial.print(l_Turntable_Motor_Position );
-          Serial.print(", Arm: ");
-          Serial.println(l_Arm_Motor_Position );
+  Serial.print("Encoders Turntable: ");
+  Serial.print(l_Turntable_Motor_Position );
+  Serial.print(", Arm: ");
+  Serial.println(l_Arm_Motor_Position );
 #endif
 }
