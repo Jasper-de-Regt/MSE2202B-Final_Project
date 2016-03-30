0d82e3726423f3b06cde05aaea1e14747aecaf59
Jasper-de-Regt <jderegt@uwo.ca>
2016-03-29 22:06:44 -0400

    Wall Following robot code using 2 ultrasonic sensors
    Wall following robot, drives along wall using 2 ultrasonic sensors

--- Code/Wall_following_bot/Wall_following_bot.ino
+++ Code/Wall_following_bot/Wall_following_bot.ino
@@ -1,36 +1,22 @@
-
-#include <PID_v1.h>
+#include <NewPing.h>
 #include <Servo.h>
-#include <uSTimer2.h>
-#include <CharliePlexM.h>
 #include <Wire.h>
 #include <I2CEncoder.h>
 
 Servo servo_rightMotor;
 Servo servo_leftMotor;
 
-//#define DEBUG_ULTRASONIC;
-
 // port pin constants
 const int rightServoPin = 8;
 const int leftServoPin = 9;
-const int ci_Ultrasonic_Data = 10;
-const int ci_Ultrasonic_Ping = 11;
+const int frontRightPingPin = 7;
+const int backRightPingPin = 4;
 
+int myspeed = 1600;
+int setpoint = 5;
 
-const int ci_Charlieplex_LED1 = 4;    //     CharliePlexM::Write(ci_Middle_Line_Tracker_LED, HIGH);
-const int ci_Charlieplex_LED2 = 5;
-const int ci_Charlieplex_LED3 = 6;
-const int ci_Charlieplex_LED4 = 7;
-const int ci_Mode_Button = 7;
-
-unsigned long ul_Echo_Time;
-
-// variables
-double setpoint, input, output;
-//double kp, ki, kd;
-
-PID myPID(&input, &output, &setpoint, 1, 0, 0, DIRECT);
+NewPing frontRightPingSensor(frontRightPingPin, frontRightPingPin, 200);
+NewPing backRightPingSensor(backRightPingPin, backRightPingPin, 200);
 
 //*********************************************************************************************************************************************************************************************************************************************************************//
 //*********************************************************************************************************************************************************************************************************************************************************************//
@@ -39,26 +25,15 @@ void setup() {
 
   Serial.begin(9600);
 
-  // setup charlieplex leds and pushbutton
-  CharliePlexM::setBtn(ci_Charlieplex_LED1, ci_Charlieplex_LED2, ci_Charlieplex_LED3, ci_Charlieplex_LED4, ci_Mode_Button);
 
   // set up drive motors
   pinMode(rightServoPin, OUTPUT);
   pinMode(leftServoPin, OUTPUT);
-  pinMode(ci_Ultrasonic_Ping, OUTPUT);
-  pinMode(ci_Ultrasonic_Data, INPUT);
+  servo_rightMotor.attach(rightServoPin);
+  servo_leftMotor.attach(leftServoPin);
 
 
-servo_rightMotor.attach(rightServoPin);
-servo_leftMotor.attach(leftServoPin);
 
-  //initiliaze pid variables
-
-  setpoint = 400;//***************************************************************//
-
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(20);
-  myPID.SetOutputLimits(50,130);
 }
 
 //*********************************************************************************************************************************************************************************************************************************************************************//
@@ -66,50 +41,79 @@ servo_leftMotor.attach(leftServoPin);
 
 void loop() {
 
+  int  frontRightSensorData = frontRightPingSensor.ping_in();
+  int backRightSensorData = backRightPingSensor.ping_in();
 
-Ping();
+  // if the correct distance away
+  if ((frontRightSensorData + backRightSensorData) / 2 == setpoint) {
+    // if straight
+    if (frontRightSensorData == backRightSensorData) {
+      //drive straight
+      servo_rightMotor.writeMicroseconds(myspeed);
+      servo_leftMotor.writeMicroseconds(myspeed);
+    }
+    //if angled towards wall
+    if (frontRightSensorData < backRightSensorData) {
+      //turn away a little
+      servo_rightMotor.writeMicroseconds(myspeed + 20);
+      servo_leftMotor.writeMicroseconds(myspeed - 20);
+    }
+    //if angled away
+    if (frontRightSensorData > backRightSensorData) {
+      // turn towards
+      servo_rightMotor.writeMicroseconds(myspeed - 20);
+      servo_leftMotor.writeMicroseconds(myspeed + 20);
+    }
+  }
 
-  // 400 seems to be a good ul_Echo_time to drive a reasonable distance away
-input = ul_Echo_Time;
-  myPID.Compute();
+  //if too close
+  if ((frontRightSensorData + backRightSensorData) / 2 < setpoint) {
+    // if parallel
+    if (frontRightSensorData == backRightSensorData) {
+      //parallel and too close, turn away slightly
+      servo_rightMotor.writeMicroseconds(myspeed + 20);
+      servo_leftMotor.writeMicroseconds(myspeed - 20);
+    }
+    //if angled towards
+    if (frontRightSensorData < backRightSensorData) {
+      //you are too close and heading closer, turn away more
+      servo_rightMotor.writeMicroseconds(myspeed + 40);
+      servo_leftMotor.writeMicroseconds(myspeed - 40);
+    }
+    // if angled away
+    if (frontRightSensorData > backRightSensorData) {
+        // thats good because your too close, drive straight
+        servo_rightMotor.writeMicroseconds(myspeed);
+        servo_leftMotor.writeMicroseconds(myspeed);
+      }
+  }
 
-//servo_leftMotor.write(85);
 
-//Serial.println(output);
+ //if too far
+  if ((frontRightSensorData + backRightSensorData) / 2 > setpoint) {
+    // if parallel
+    if (frontRightSensorData == backRightSensorData) {
+      //parallel and too far, turn closer slightly
+      servo_rightMotor.writeMicroseconds(myspeed - 20);
+      servo_leftMotor.writeMicroseconds(myspeed + 20);
+    }
+    //if angled towards
+    if (frontRightSensorData < backRightSensorData) {
+      //you are too far and heading closer, go straight
+      servo_rightMotor.writeMicroseconds(myspeed);
+      servo_leftMotor.writeMicroseconds(myspeed);
+    }
+    // if angled away
+    if (frontRightSensorData > backRightSensorData) {
+        // too far and heading further away, turn in a bunch
+        servo_rightMotor.writeMicroseconds(myspeed-40);
+        servo_leftMotor.writeMicroseconds(myspeed+40);
+      }
+  }
 
-servo_rightMotor.write(output);
-servo_leftMotor.write(70);
-
-Serial.println();
-Serial.print("input  ");
-Serial.print(input);
-Serial.print("   output   ");
-Serial.print(output);
-
-}
 
 
 
 
-void Ping()
-{
-  //Ping Ultrasonic
-  //Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
-  digitalWrite(ci_Ultrasonic_Ping, HIGH);
-  delayMicroseconds(10);  //The 10 microsecond pause where the pulse in "high"
-  digitalWrite(ci_Ultrasonic_Ping, LOW);
-  //use command pulseIn to listen to Ultrasonic_Data pin to record the
-  //time that it takes from when the Pin goes HIGH until it goes LOW
-  ul_Echo_Time = pulseIn(ci_Ultrasonic_Data, HIGH, 10000);
 
-  // Print Sensor Readings
-#ifdef DEBUG_ULTRASONIC
-  Serial.print("Time (microseconds): ");
-  Serial.print(ul_Echo_Time, DEC);
-  Serial.print("   HI MEGAN!!    "):
-  Serial.print(", Inches: ");
-  Serial.print(ul_Echo_Time / 148); //divide time by 148 to get distance in inches
-  Serial.print(", cm: ");
-  Serial.println(ul_Echo_Time / 58); //divide time by 58 to get distance in cm
-#endif
 }
