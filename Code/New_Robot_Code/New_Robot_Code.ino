2849471e2f56dbd492ccca3b42f75902a9a769e9
mhende29 <mhende29@uwo.ca>
2016-04-01 15:08:28 -0400

    going strong

--- Code/New_Robot_Code/New_Robot_Code.ino
+++ Code/New_Robot_Code/New_Robot_Code.ino
@@ -5,6 +5,32 @@
 #include <Wire.h>
 #include <I2CEncoder.h>
 
+/*
+  Naming conventions:
+
+  camelcase
+  - E.g. itLooksLikeThis
+
+  significant words in the title should be separated by underscores "_"
+  - E.g. motor_pin_one
+
+  CAPITALIZE ACRONYMS
+  - E.g. IR instead of infrared.
+       LED instead of light_emitting_diode or lightEmittingDiode
+
+*/
+
+Servo servo_left_motor;
+Servo servo_right_motor;
+Servo servo_turntable_motor;
+Servo servo_arm_motor;
+Servo servo_wrist_motor;
+Servo servo_magnet_motor;
+
+I2CEncoder encoder_leftMotor;
+I2CEncoder encoder_rightMotor;
+I2CEncoder encoder_turntable_motor;
+I2CEncoder encoder_arm_motor;
 
 //port pin constants
 //digital pins
@@ -22,9 +48,41 @@ const int ci_hall_effect = A3;
 const int ci_I2C_SDA = A4;         // I2C data = white
 const int ci_I2C_SCL = A5;         // I2C clock = yellow
 
-void setup() {
-  // put your setup code here, to run once:
+//Position constants-------Set to 0 if unknown at the moment
+//Turntable positions
+const int ci_turntable_default_position = 0;     //  Experiment to determine appropriate value
+const int ci_turntable_left_position = 400;
+const int ci_turntable_middle_position = 980;
+const int ci_turntable_right_position = 1540;
 
+//Arm positions
+const int ci_arm_vertical_position = 400;
+const int ci_arm_diagonal_position = 200;
+const int ci_arm_horizontal_position = 0;
+const int ci_arm_modetwo_dropoff = 0;
+
+//Wrist positions
+const int ci_wrist_position_vertical = 0;    //  " Wrist bar is perpendicular to the arm.
+const int ci_wrist_position_diagonal = 0;    //45 degree angle
+const int ci_wrist_position_horizontal = 0;  //  " Wrist bar is parallel to the arm.
+const int ci_wrist_modetwo_dropoff = 0;      //  "
+
+//Magnet servo positions
+const int ci_magnet_up_position = 0; // Cant pickup tesseracts
+const int ci_magnet_down_position = 0; //Will pickup tesseracts
+
+boolean bt_IRcrown_detection;                 //  " logic is backwards ie. false is positive and true is negative
+//bt_IRcrown_detection=digitalRead(ci_IR_crown); //I think
+
+//for driving
+const double cd_robot_diameter = 23.42;          //  Radius of the device ~ 23.42 mm
+char ch_tracking_direction = 'R';                //  Character value is either 'R', 'L', 'l' or 'r'
+unsigned int ui_num_turns = 0;
+const int ci_drive_speed = 1600;
+
+void setup() {
+  Wire.begin();        // Wire library required for I2CEncoder library
+  Serial.begin(9600);
 }
 
 void loop() {
