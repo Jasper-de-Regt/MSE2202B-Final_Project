c71532e38e6ca56ea639c0ff50dab1d473845b69
mhende29 <mhende29@uwo.ca>
2016-04-01 16:23:51 -0400

    started testing RIP

--- Code/New_Robot_Code/New_Robot_Code.ino
+++ Code/New_Robot_Code/New_Robot_Code.ino
@@ -106,6 +106,10 @@ void setup() {
   pinMode(ci_arm_motor, OUTPUT);
   arm_motor.attach(ci_arm_motor);
 
+  pinMode(ci_big_wrist_servo, OUTPUT);
+  servo_wrist_motor.attach(ci_big_wrist_servo);
+
+
   //************************************************************************
 
   // setup encoders. Must be initiliazed in the order that they are chained together,
@@ -124,18 +128,21 @@ void setup() {
 
   encoder_turntable_motor.zero();       //Robot arm must be positioned on the standoffs to be properly zeroed
   encoder_arm_motor.zero();
+  delay(1000);
 }
+
+
+
+
+
+
+
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  armEncoderPosition(ci_arm_diagonal_position);
 }
-
-
-
-
-
-
 
 
 
@@ -150,25 +157,25 @@ void loop() {
 
 /* Function List + How to Call
 
-Arm Servo functions
-void moveToPosn(servo, servo pin, initial posn, final posn, bool to start at initial);
+  Arm Servo functions
+  void moveToPosn(servo, servo pin, initial posn, final posn, bool to start at initial);
 
-Driving functions
-void stopDrive(); //Stops the motors
-void driveStraight(int driveSpeed); //Sets both motors at the same speed
-void driveStraightAheadEncoders(int driveSpeed, int encoderTicks); //driveSpeed is a constant, encoder ticks corresponds to the distance you want to travel. **1000 encoder ticks makes for about 15.5" or 39.4cm
+  Driving functions
+  void stopDrive(); //Stops the motors
+  void driveStraight(int driveSpeed); //Sets both motors at the same speed
+  void driveStraightAheadEncoders(int driveSpeed, int encoderTicks); //driveSpeed is a constant, encoder ticks corresponds to the distance you want to travel. **1000 encoder ticks makes for about 15.5" or 39.4cm
 
-Wall-Following functions
-void followWall(int driveSpeed, char wallSide, int desiredDistance); // call to follow a wall. Example followWall(1600, R, 15) will follow a wall on the right side, maintaining a distance of 15cm, at a speed of 1600
-void moveFurtherFromWall(int driveSpeed, char wallSide); //wallSide should be either R or L (capital or lowercase)
+  Wall-Following functions
+  void followWall(int driveSpeed, char wallSide, int desiredDistance); // call to follow a wall. Example followWall(1600, R, 15) will follow a wall on the right side, maintaining a distance of 15cm, at a speed of 1600
+  void moveFurtherFromWall(int driveSpeed, char wallSide); //wallSide should be either R or L (capital or lowercase)
 
-Turning functions
-void turnRight(int driveSpeed, int speedModifier); //driveSpeed is const. speedModifier is added/subtracted from left/right respectively.
-void turnRightSharp(int driveSpeed, int speedModifier); //similar to turnRight(), but speedModifier is multiplied by 1.5
-void turnLeft(int driveSpeed, int speedModifier); //driveSpeed is const. speedModifier is added/subtracted from right/left respectively.
-void turnLeftSharp(int driveSpeed, int speedModifier); //similar to turnLeft(), but speedModifier is multiplied by 1.5
-void skidsteerNinetyRight(int driveSpeed); // driveSpeed will be a constant as defined in the main code 
-void skidsteerNinetyLeft(int driveSpeed) // Same note as above
+  Turning functions
+  void turnRight(int driveSpeed, int speedModifier); //driveSpeed is const. speedModifier is added/subtracted from left/right respectively.
+  void turnRightSharp(int driveSpeed, int speedModifier); //similar to turnRight(), but speedModifier is multiplied by 1.5
+  void turnLeft(int driveSpeed, int speedModifier); //driveSpeed is const. speedModifier is added/subtracted from right/left respectively.
+  void turnLeftSharp(int driveSpeed, int speedModifier); //similar to turnLeft(), but speedModifier is multiplied by 1.5
+  void skidsteerNinetyRight(int driveSpeed); // driveSpeed will be a constant as defined in the main code
+  void skidsteerNinetyLeft(int driveSpeed) // Same note as above
 
 */
 
@@ -305,18 +312,28 @@ void armEncoderPosition(int encoderPosition) {
 
 
 void tesseractScanSweep(int maxPosition) {
-  for (int i = encoder_turntable_motor.getRawPosition(); i < maxPosition; i + 10) {
-    for (int i = encoder_turntable_motor.getRawPosition(); i < maxPosition; i + 30) { // not sure if this is the best way to scan
-      turnTurntableEncodersPosition(i);
-      if (analogRead(ci_hall_effect)) {     //checks to see if there is an abnormality in the hall effects analog read, if yes then break the loop
-        break;
-      }
-      armEncoderPosition(ci_arm_diagonal_position);
+  for (int i = encoder_turntable_motor.getRawPosition(); i < maxPosition; i + 30) { // not sure if this is the best way to scan
+    turnTurntableEncodersPosition(i);
+    if (analogRead(ci_hall_effect)) {     //checks to see if there is an abnormality in the hall effects analog read, if yes then break the loop
+      break;
     }
+    armEncoderPosition(ci_arm_diagonal_position);
   }
 }
 
-void moveToPosition(Servo serv0, int final_Position) {
+/*void badTesseractSweep(){
+  for (int i = encoder_turntable_motor.getRawPosition(); i < maxPosition; i + 30) { // not sure if this is the best way to scan
+    turnTurntableEncodersPosition(i);
+    if (analogRead(ci_hall_effect)) {     //checks to see if there is an abnormality in the hall effects analog read, if yes then break the loop
+      break;
+    }
+    armEncoderPosition(ci_arm_diagonal_position);
+  }
+}
+*/
+
+
+void servoMoveToPosition(Servo serv0, int final_Position) {
   long previous = millis();
   while (serv0.read() != final_Position) {
     if ((millis() - previous) >= timeDifference) {
@@ -325,7 +342,7 @@ void moveToPosition(Servo serv0, int final_Position) {
       }
       if (serv0.read() < final_Position) {
         serv0.write(serv0.read() + 1);
-      } 
+      }
       previous = millis();
     }
   }
